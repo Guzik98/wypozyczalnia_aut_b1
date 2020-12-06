9f82117826540152326f56ace254377502fdec01
Guzik98 <guzkac2@gmail.com>
2020-12-06 21:19:32 +0100

    wysyam ponownie

--- wypozyczalnia_aut_b1/settings.py
+++ wypozyczalnia_aut_b1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=#obtaxyv#hw)ba!kveh^bdkjr*w+h%dd^j_5uk!^9zy24$muo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','pythonanywhere']
 
 
 # Application definition
