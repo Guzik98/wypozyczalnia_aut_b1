e841abf4db88db2868c833bba0b36976357daddd
CichyZmywacz <michal831201@gmail.com>
2020-12-14 13:24:20 +0100

    [Reset hasla] #WYA-35

--- wypozyczalnia_aut_b1/settings.py
+++ wypozyczalnia_aut_b1/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'wypozyczalnia-home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'grupawypozyczalniab1@gmail.com'
+EMAIL_HOST_PASSWORD = 'zaq1@WSX'
